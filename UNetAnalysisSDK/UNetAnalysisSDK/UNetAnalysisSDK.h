3ab5bc86ebc47c712f36b0449e4a4a0e2ca736d3
ethan.zhang <ethan.zhang@ucloud.cn>
2019-01-03 16:14:36 +0800

    update public file

--- UNetAnalysisSDK/UNetAnalysisSDK/UNetAnalysisSDK.h
+++ UNetAnalysisSDK/UNetAnalysisSDK/UNetAnalysisSDK.h
@@ -17,3 +17,6 @@ FOUNDATION_EXPORT const unsigned char UCloudNetAnalysisSDKVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <UCloudNetAnalysisSDK/PublicHeader.h>
 
 #import <UNetAnalysisSDK/UCNetAnalysisManager.h>
+#import <UNetAnalysisSDK/UCErrorModel.h>
+#import <UNetAnalysisSDK/UCManualNetDiagResult.h>
+#import <UNetAnalysisSDK/UCModel.h>
